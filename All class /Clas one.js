db82dab6581773ec277cd00af71130a6dbc5d7ba
Sadikur Rhman <sadikurrhman6530@gmail.com>
2026-01-05 17:28:42 +0600

    Update and rename Clas one to Clas one.js

---
+++ All class /Clas one.js
@@ -0,0 +1,7 @@
+// array niya kaj korte hole amake prothome jante hobe kmne arry miya kaj kora jay bolte gele array bitore kmne ki rakha lage mane assain 
+// array initialize korar kisu pokriya roye se tar maje sop thke sohoj maddrom holo
+
+// const array_name = [arry_ 1, arry_2, etc]
+
+// a_r_r_a_y literal 
+
