0d98d07fe533ce30e0b0ac988a4f0b9c59789d9f
Sadikur Rhman <sadikurrhman6530@gmail.com>
2026-01-14 22:46:43 +0600

    Create main.js

---
+++ 013.Concat Multiple Arrays/main.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3]
+const arr1 = [4, 5, 6]
+
+const arr2 = arr.concat(arr1)
+const arr3 = [... arr, ...arr1]
+const arr4 = [].concat(arr, arr1, 7, 8, 9, 10)
+console.log(arr2) // [ 1, 2, 3, 4, 5, 6 ]
+console.log(arr3) // [ 1, 2, 3, 4, 5, 6 ]
+
+comsole.log(arr4) /** [
+  1, 2, 3, 4,  5,
+  6, 7, 8, 9, 10
+]*/
