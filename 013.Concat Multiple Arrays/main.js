143a0f33422742333e6ddfdd245577755810c9b2
Sadikur Rhman <sadikurrhman6530@gmail.com>
2026-01-14 22:50:40 +0600

    Update main.js

--- 013.Concat Multiple Arrays/main.js
+++ 013.Concat Multiple Arrays/main.js
@@ -7,7 +7,7 @@ const arr4 = [].concat(arr, arr1, 7, 8, 9, 10)
 console.log(arr2) // [ 1, 2, 3, 4, 5, 6 ]
 console.log(arr3) // [ 1, 2, 3, 4, 5, 6 ]
 
-comsole.log(arr4) /** [
+console.log(arr4) /** [
   1, 2, 3, 4,  5,
   6, 7, 8, 9, 10
 ]*/
